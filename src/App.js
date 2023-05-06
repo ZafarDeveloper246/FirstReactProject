9debe179216109fea1712df64df53a0754401687
arslan <arslan@gmail.com>
2023-05-06 19:29:56 +0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar'
+
+function App() {
+
+  console.log("asdcs");
+  return (
+    <>
+    <Navbar/>
+ <h1>Arslan Akmal</h1>
+ </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import "./Navbar.css"
+function Navbar() {
+  return (
+    <div>
+      <h1 className="title">Hello From Navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
